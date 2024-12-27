555b27a5c19959091cf7953b4e54d25d82f34f74
notcurrentuser <notcurrentuser@gmail.com>
2024-12-27 13:55:22 +0200

    feat: added Vote model & Vote repository

---
+++ core/models/Vote.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from typing import Optional
+from datetime import datetime
+
+@dataclass
+class Vote:
+    voter_id: str
+    candidate_id: str
+    timestamp: datetime
+    blockchain_tx_hash: Optional[str] = None
---
+++ core/repositories/VoteRepositories.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from typing import List
+from core.models import Vote
+
+class VoteRepository(ABC):
+    @abstractmethod
+    def save(self, vote: Vote) -> None:
+        pass
+
+    @abstractmethod
+    def get_votes(self) -> List[Vote]:
+        pass
